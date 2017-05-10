34f6338cacb27a3f9bae0a1f23625f34a5382bf4
S500CA-HPD0101N <xydfayt@yahoo.com>
2017-05-10 15:49:36 +0800

    may10

---
+++ app/src/main/java/tk/urbantaxi/txiapp/Login.java
@@ -0,0 +1,19 @@
+package tk.urbantaxi.txiapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
